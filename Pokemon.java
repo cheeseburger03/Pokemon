ba3477f638917453d2f89a1d978664068094f67e
cheeseburger03 <60773307+cheeseburger03@users.noreply.github.com>
2020-02-07 15:28:58 +0700

    First commit

---
+++ Pokemon.java
@@ -0,0 +1,12 @@
+public class Pokemon{
+        class pokemon_cs {
+            private String name ;
+            private String pokemonType ;
+            private int health ;
+            private int max_health ;
+            private int level ;
+            private int PP ;
+        }
+    
+
+}
